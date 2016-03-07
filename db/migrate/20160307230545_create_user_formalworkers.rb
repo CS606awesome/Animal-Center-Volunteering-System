3699da60fe990f70242e85613da557f10293e0fa
juliaaaa2015 <dongnihan@hotmail.com>
2016-03-07 23:31:18 +0000

    columns

---
+++ db/migrate/20160307230545_create_user_formalworkers.rb
@@ -0,0 +1,12 @@
+class CreateUserFormalworkers < ActiveRecord::Migration
+  def change
+    create_table :user_formalworkers do |t|
+      t.datetime date_of_employment
+      t.string reason_for_leaving
+      t.string position/department
+      t.string interested_area
+
+      t.timestamps null: false
+    end
+  end
+end
