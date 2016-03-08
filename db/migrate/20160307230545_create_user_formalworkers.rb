84b0e7b765622066752b1f3ea7ae9287d2d3eb6f
Dongni Han <dongnihan@hotmail.com>
2016-03-07 22:25:22 -0600

    Update 20160307230545_create_user_formalworkers.rb

--- db/migrate/20160307230545_create_user_formalworkers.rb
+++ db/migrate/20160307230545_create_user_formalworkers.rb
@@ -3,7 +3,7 @@ class CreateUserFormalworkers < ActiveRecord::Migration
     create_table :user_formalworkers do |t|
       t.datetime :date_of_employment
       t.string :reason_for_leaving
-      t.string :position/department
+      t.string :position_or_department
       t.string :interested_area
 
       t.timestamps null: false
