6f62d600abb4bb78e2c7c680871c732cc3098d23
Dongni Han <dongnihan@hotmail.com>
2016-03-07 22:26:07 -0600

    Update 20160307231100_create_accommodations.rb

--- db/migrate/20160307231100_create_accommodations.rb
+++ db/migrate/20160307231100_create_accommodations.rb
@@ -1,7 +1,7 @@
 class CreateAccommodations < ActiveRecord::Migration
   def change
     create_table :accommodations do |t|
-      t.string accommodation_name
+      t.string :accommodation_name
       t.timestamps null: false
     end
   end
