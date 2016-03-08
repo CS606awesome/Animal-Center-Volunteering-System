a0170cbecb3139419886a180b186c799eea15c29
zhangsikai123 <Zhangsikai123@tamu.edu>
2016-03-07 23:03:13 -0600

    yes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307230636) do
+ActiveRecord::Schema.define(version: 20160307231855) do
+
+  create_table "accommodations", force: :cascade do |t|
+    t.string   "accommodation_name", limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
 
   create_table "accounts", force: :cascade do |t|
     t.string  "email",                     limit: 255
@@ -33,6 +39,72 @@ ActiveRecord::Schema.define(version: 20160307230636) do
     t.binary  "picture",                   limit: 16777215
     t.binary  "driver_license",            limit: 16777215
     t.integer "SSN",                       limit: 4
+  end
+
+  create_table "application_forms", force: :cascade do |t|
+    t.string   "interested_areas",     limit: 255
+    t.string   "volunteering_status",  limit: 255
+    t.binary   "signature",            limit: 16777215
+    t.datetime "application_date"
+    t.datetime "available_time_begin"
+    t.datetime "available_time_end"
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+  end
+
+  create_table "criminal_applications", force: :cascade do |t|
+    t.integer  "mandatory_hours", limit: 4
+    t.string   "mandatory_area",  limit: 255
+    t.datetime "deadlin"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  create_table "current_workers", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "department", limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "formal_criminals", force: :cascade do |t|
+    t.datetime "date_of_conviction"
+    t.string   "nature_of_offense",   limit: 255
+    t.string   "name_of_court",       limit: 255
+    t.string   "disposition_of_case", limit: 255
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  create_table "minor_applications", force: :cascade do |t|
+    t.binary   "parent_signature", limit: 16777215
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+  end
+
+  create_table "related_councilmembers", force: :cascade do |t|
+    t.string   "name",         limit: 255
+    t.string   "relationship", limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  create_table "student_applications", force: :cascade do |t|
+    t.string   "student_program", limit: 255
+    t.integer  "required_time",   limit: 4
+    t.string   "required_area",   limit: 255
+    t.datetime "deadline"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  create_table "user_formalworkers", force: :cascade do |t|
+    t.datetime "date_of_employment"
+    t.string   "reason_for_leaving",     limit: 255
+    t.string   "position_or_department", limit: 255
+    t.string   "interested_area",        limit: 255
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
   end
 
 end
