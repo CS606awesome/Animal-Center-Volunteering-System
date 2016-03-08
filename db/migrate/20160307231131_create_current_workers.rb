5e3542f83c31c5bbd45d44e1e8ddd28f836c8251
Dongni Han <dongnihan@hotmail.com>
2016-03-07 22:26:23 -0600

    Update 20160307231131_create_current_workers.rb

--- db/migrate/20160307231131_create_current_workers.rb
+++ db/migrate/20160307231131_create_current_workers.rb
@@ -1,8 +1,8 @@
 class CreateCurrentWorkers < ActiveRecord::Migration
   def change
     create_table :current_workers do |t|
-      t.string name
-      t.string department
+      t.string :name
+      t.string :department
       t.timestamps null: false
     end
   end
