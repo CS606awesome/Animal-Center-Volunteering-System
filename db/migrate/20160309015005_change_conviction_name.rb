e405b559be0def68326b611cc8a9a2a42024b912
zhangsikai123 <Zhangsikai123@tamu.edu>
2016-03-11 14:20:31 -0600

    Database change to sqlite
    change db from mysql to sqlite3

--- db/migrate/20160309015005_change_conviction_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeConvictionName < ActiveRecord::Migration
-  def change
-    rename_column :accounts, :has_convicitons, :has_convictions
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -14,98 +14,98 @@
 ActiveRecord::Schema.define(version: 20160309021705) do
 
   create_table "accommodations", force: :cascade do |t|
-    t.string   "accommodation_name", limit: 255
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.string   "accommodation_name"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "accounts", force: :cascade do |t|
-    t.string  "email",                     limit: 255
-    t.string  "firstname",                 limit: 255
-    t.string  "lastname",                  limit: 255
-    t.string  "password_digest",           limit: 255
-    t.string  "current_address",           limit: 255
-    t.string  "homephone",                 limit: 255
-    t.string  "cellphone",                 limit: 255
-    t.string  "emergency_contact_name",    limit: 255
-    t.string  "emergency_phone",           limit: 255
-    t.string  "emergency_phone_alternate", limit: 255
+    t.string  "email"
+    t.string  "firstname"
+    t.string  "lastname"
+    t.string  "password_digest"
+    t.string  "current_address"
+    t.string  "homephone"
+    t.string  "cellphone"
+    t.string  "emergency_contact_name"
+    t.string  "emergency_phone"
+    t.string  "emergency_phone_alternate"
     t.date    "DOB"
-    t.boolean "is_formal_worker",          limit: 1
-    t.boolean "has_convictions",           limit: 1
-    t.boolean "need_accomendations",       limit: 1
-    t.boolean "related_to_councilmember",  limit: 1
-    t.boolean "is_current_worker",         limit: 1
-    t.binary  "picture",                   limit: 16777215
-    t.binary  "driver_license",            limit: 16777215
-    t.integer "SSN",                       limit: 4
-    t.string  "middlename",                limit: 255
+    t.boolean "is_formal_worker"
+    t.boolean "has_convictions"
+    t.boolean "need_accomendations"
+    t.boolean "related_to_councilmember"
+    t.boolean "is_current_worker"
+    t.binary  "picture"
+    t.binary  "driver_license"
+    t.integer "SSN"
+    t.string  "middlename"
   end
 
   create_table "application_forms", force: :cascade do |t|
-    t.string   "interested_areas",     limit: 255
-    t.string   "volunteering_status",  limit: 255
-    t.binary   "signature",            limit: 16777215
+    t.string   "interested_areas"
+    t.string   "volunteering_status"
+    t.binary   "signature"
     t.datetime "application_date"
     t.datetime "available_time_begin"
     t.datetime "available_time_end"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   create_table "criminal_applications", force: :cascade do |t|
-    t.integer  "mandatory_hours", limit: 4
-    t.string   "mandatory_area",  limit: 255
+    t.integer  "mandatory_hours"
+    t.string   "mandatory_area"
     t.datetime "deadlin"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "current_workers", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "department", limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.string   "department"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "formal_criminals", force: :cascade do |t|
     t.datetime "date_of_conviction"
-    t.string   "nature_of_offense",   limit: 255
-    t.string   "name_of_court",       limit: 255
-    t.string   "disposition_of_case", limit: 255
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.string   "nature_of_offense"
+    t.string   "name_of_court"
+    t.string   "disposition_of_case"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
 
   create_table "minor_applications", force: :cascade do |t|
-    t.binary   "parent_signature", limit: 16777215
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.binary   "parent_signature"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "related_councilmembers", force: :cascade do |t|
-    t.string   "name",         limit: 255
-    t.string   "relationship", limit: 255
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "name"
+    t.string   "relationship"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "student_applications", force: :cascade do |t|
-    t.string   "student_program", limit: 255
-    t.integer  "required_time",   limit: 4
-    t.string   "required_area",   limit: 255
+    t.string   "student_program"
+    t.integer  "required_time"
+    t.string   "required_area"
     t.datetime "deadline"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "user_formalworkers", force: :cascade do |t|
     t.datetime "date_of_employment"
-    t.string   "reason_for_leaving",     limit: 255
-    t.string   "position_or_department", limit: 255
-    t.string   "interested_area",        limit: 255
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.string   "reason_for_leaving"
+    t.string   "position_or_department"
+    t.string   "interested_area"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
