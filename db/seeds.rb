dc3774ebe6db39302244ebbc58a045d1598ea7b7
Artoria Pendragon <huajiefeizhu@gmail.com>
2016-03-14 21:43:30 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+accounts =  [{:email => '423523@qwe.com',
+             :firstname => 'xudong',
+             :lastname => 'huang',
+             :password_digest => '12345' ,
+             :current_address => 'baker street',
+             :homephone => '4543656345',
+             :cellphone => '54356565'}]
+
+accounts.each do |account|
+  Account.create!(account)
+end
+             
+             # "emergency_contact_name" 'yifeng',
+             # "emergency_phone_alternate" '2222222',
+             # "DOB" '2000-1-1',
+             # "is_formal_worker" boolean,
+             # "has_convictions" boolean,
+             # "need_accomendations" boolean,
+             # "related_to_councilmember" boolean,
+             # "is_current_worker" boolean,
+             # "picture" blob(2097152),
+             # "driver_license" blob(2097152),
+             # "SSN" integer,
+             # "middlename" varchar
