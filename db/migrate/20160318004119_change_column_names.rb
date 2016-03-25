e20beee581d651969e85907f439978759b0a5109
zhangsikai123 <Zhangsikai123@tamu.edu>
2016-03-25 13:03:59 -0500

    db update

--- db/migrate/20160318004119_change_column_names.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNames < ActiveRecord::Migration
-  def change
-  	 rename_column :accounts, :is_formal_worker, :is_former_worker
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318004119) do
+ActiveRecord::Schema.define(version: 20160318003939) do
 
   create_table "accommodations", force: :cascade do |t|
     t.string   "accommodation_name"
