bf35312321cb07c7442d0dccd8d32e4a0af6ec22
zhangsikai123 <Zhangsikai123@tamu.edu>
2016-03-31 01:14:27 -0500

    db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160330223350) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accommodations", force: :cascade do |t|
     t.string   "accommodation_name"
     t.datetime "created_at",         null: false
