a1306fe381b0a41ca5022586c356fd5cf460adce
zhangsikai123 <Zhangsikai123@tamu.edu>
2016-03-30 22:14:52 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-     root 'index#index'
+  root 'accounts#new'
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
