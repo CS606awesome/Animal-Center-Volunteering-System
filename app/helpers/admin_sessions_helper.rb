2a354b29ea7db4a781697a2350c82b0c08dfd9e3
zhangsikai123 <Zhangsikai123@tamu.edu>
2016-04-05 12:22:50 -0500

    modi

--- app/helpers/admin_sessions_helper.rb
+++ app/helpers/admin_sessions_helper.rb
@@ -1,11 +1,11 @@
 module AdminSessionsHelper
 	 def admin_log_in(admin)
-    session[:adminid] = admin.id
+    session[:id] = admin.id
      end
      def admin_logged_in
-     	session[:adminid]
+     	session[:id]
      end
      def admin_log_out
-     session.delete(:adminid)
+     session.delete(:id)
   end
 end
