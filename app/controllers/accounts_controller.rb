a220f2a717f0f123717e58dafa13fd49e0b7aafb
zyjoey123 <windwhisperzy@gmail.com>
2016-04-20 20:59:33 +0000

    caneditprofiles

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -252,6 +252,7 @@ private
    params.require(:account).permit(:is_former_worker,:is_current_worker, :emergency_contact_name,
                                   :emergency_phone,:emergency_phone_alternate,:related_to_councilmember,
                                   :has_convictions, :need_accommodations, :is_volunteering,  :is_student,
+                                  :firstname, :lastname, :DOB, :homephone, :cellphone, :street, :city, :state, :zip,
                                   current_worker_attributes: [:id, :department, :name],
                                   user_formerworker_attributes: [:id, :date_of_employment, :reason_for_leaving, :position_or_department],
                                   former_criminal_attributes: [:id, :date_of_conviction, :nature_of_offense, :name_of_court, :disposition_of_case, :former_crime],
