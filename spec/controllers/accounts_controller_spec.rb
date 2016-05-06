1601f7a4d855f9985dd70368f40d42bb3632f0df
zyjoey123 <windwhisperzy@gmail.com>
2016-05-06 00:52:14 +0000

    tdd step 1

---
+++ spec/controllers/accounts_controller_spec.rb
@@ -0,0 +1,84 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe AccountsController do 
+    before :each do
+    accounts = [{:firstname => 'Xudong', 
+                 :lastname => 'Huang',
+  	             :DOB => '1999-01-01', 
+  	             :email => 'xudong1@qq.com',
+                 :password => '12345678',
+                 :homephone => '111-111-1111',
+                 :cellphone => '222-222-2222',
+                 :gender =>'male'
+                }
+  	           ]
+
+    @accounts = accounts.map { |account| Account.create! account } 
+  end
+  
+  describe "GET #profiles" do
+
+    
+    it "it renders login template" do 
+      get :profiles
+      expect(subject).should redirect_to login_path
+    end
+    
+    it "goes to profile page" do 
+      session[:id] = @accounts[0].id
+      get :profiles
+      response.body.should == ""
+    end
+    
+    it "get correct profile" do 
+      session[:id] = @accounts[0].id
+      get :profiles
+      assigns(:account).should eq @accounts[0]
+    end
+  end
+  
+  describe "GET #application" do 
+    it "it renders login template" do 
+      get :application
+      expect(subject).should redirect_to login_path
+    end
+    
+    it "find correct application account" do 
+      session[:id] = @accounts[0].id
+      get :application
+      assigns(:account).should eq @accounts[0]
+    end
+  end
+  
+  describe "GET #viewapplication" do 
+    it "it renders login template" do 
+      get :viewapplication
+      expect(subject).should redirect_to login_path
+    end
+    
+    it "find correct viewapplication account" do 
+      session[:id] = @accounts[0].id
+      get :viewapplication
+      assigns(:account).should eq @accounts[0]
+    end
+  end  
+  
+  describe "GET #save_change" do 
+    
+    it "find account and save" do 
+      session[:id] = @accounts[0].id
+      get :save_change
+      assigns(:account).should eq @accounts[0]
+    end
+  end
+  
+  # describe " #update" do
+    # it "change saved" do 
+      # session[:id] = @accounts[0].id
+      # put :update
+      # flash[:danger].should == 'Changes Saved!'
+    # end
+  # end
+    
+end
